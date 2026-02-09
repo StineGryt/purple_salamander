f10dcd428716a93c6358bf8d58e2004efbb49fbc
StineGryt <stineemigry@gmail.com>
2026-02-09 13:07:38 +0100

    palmer altered 13:07 09.02.2026

--- palmer_penguins_090226.R
+++ palmer_penguins_090226.R
@@ -1,17 +1,17 @@
 
-# Load packages
+# ---Load packages---
 library(palmerpenguins)
 library(tidyverse)
 library(ggplot2)
 
 
-# Plot
-mass_flipper <- ggplot(data = penguins, 
+# ---Plot---
+mass_flipper <- ggplot(data = palmerpenguins::penguins, 
                        aes(x = flipper_length_mm,
                            y = body_mass_g)) +
   geom_point(aes(color = species, 
                  shape = species),
-             size = 3,
+             size = 2.5,
              alpha = 0.7)
 
 mass_flipper
